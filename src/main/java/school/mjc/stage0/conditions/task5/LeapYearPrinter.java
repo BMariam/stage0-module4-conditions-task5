def436bac230a18085624e7e0661b59e51fb6220
BMariam <mariambaghdasaryan77@gmail.com>
2023-08-04 21:06:54 +0400

    Updated files

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+		if (0 == year % 4) {
+			System.out.println("leap");
+		} else {
+			System.out.println("not leap");
+		}
     }
 }
